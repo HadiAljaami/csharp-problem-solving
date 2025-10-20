497f5b9280f9657c3b880ce6c1225c04c67d8cbc
hadi <aljaami.hadi@gmail.com>
2025-10-20 15:51:22 +0300

    02_ReadAndPrintName

---
+++ Level-01/02-ReadAndPrintName/02-ReadAndPrintName/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using System.Runtime.CompilerServices;
+
+namespace _02_ReadAndPrintName
+{
+    internal class Program
+    {
+        static bool IsValidName(string name)
+        {
+            return !string.IsNullOrWhiteSpace(name);
+        }
+
+        static string ReadName()
+        {
+            string Name= string.Empty;
+            bool isValid = false;
+
+
+
+            Console.WriteLine("Enter Your Name Please : ");
+
+            do 
+            {
+                Name = Console.ReadLine()!;
+                 isValid = IsValidName(Name);
+                if (!isValid)
+                {
+                    Console.WriteLine("Name cannot be empty. Please enter your name:");
+                }
+            }
+            while (!isValid);
+  
+            return Name;
+        }
+
+        static string  PrintName(string name)
+        {
+            return name;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My Name Is :" + PrintName(ReadName()));
+        }
+    }
+}

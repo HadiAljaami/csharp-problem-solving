4ca713837375e1a015c8ac0037d9628d8c757e96
hadi <aljaami.hadi@gmail.com>
2025-10-20 15:16:18 +0300

    Added problem-01 From Level 1

---
+++ Level-01/01-Print Name/01-Print Name/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _01_Print_Name
+{
+    internal class Program
+    {
+        static string PrintName(string Name)=> Name;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My Name Is :"+PrintName("Hadi Ameen"));
+        }
+    }
+}
